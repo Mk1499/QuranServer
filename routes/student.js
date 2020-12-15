42712779a7bfbc6557290ff320335e2d3a34d07a
MK1499 <m.khaled1499@gmail.com>
2020-12-15 14:10:21 +0200

    Un-Enroll student from teacher

--- routes/student.js
+++ routes/student.js
@@ -13,59 +13,59 @@ studentRouter.post("/add", (req, res) => {
       email: req.body.email,
       password: req.body.password,
       avatar: req.body.avatar,
-      role:'student'
+      role: "student",
     },
 
-    (err,student) => {
+    (err, student) => {
       if (err) {
         res.status(400).json({ message: "Student Not Created", error: err });
       } else {
-        res.status(200).json({ message: "Student Created" , student});
+        res.status(200).json({ message: "Student Created", student });
       }
     }
   );
 });
 
-
-studentRouter.get("/",(req,res)=>{
-  Student.find({},(err,data)=>{
+studentRouter.get("/", (req, res) => {
+  Student.find({}, (err, data) => {
     if (err) {
       res.status(400).json({ message: "Students Not Listed", error: err });
     } else {
       res.status(200).json({ students: data });
     }
-  })
-})
+  });
+});
 
-studentRouter.get("/:id",(req,res)=>{
-  Student.find({_id:req.params.id},(err,data)=>{
+studentRouter.get("/:id", (req, res) => {
+  Student.find({ _id: req.params.id }, (err, data) => {
     if (err) {
       res.status(400).json({ message: "Students Not Listed", error: err });
     } else {
       res.status(200).json({ students: data });
     }
-  })
-})
+  });
+});
 
-studentRouter.post("/login",(req,res)=>{
-  let email = req.body.email; 
-  let password = req.body.password; 
+studentRouter.post("/login", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
 
-  Student.findOne({
-    email, 
-    password
-  },(err,data)=>{
-    if(err){
-      res.status(400).json({ message: "An Error Happened", error: err });
-    } else if(data){
-      res.status(200).json({ student: data });
+  Student.findOne(
+    {
+      email,
+      password,
+    },
+    (err, data) => {
+      if (err) {
+        res.status(400).json({ message: "An Error Happened", error: err });
+      } else if (data) {
+        res.status(200).json({ student: data });
+      } else {
+        res.status(404).json({ message: "Wrong email or password" });
+      }
     }
-    else {
-      res.status(404).json({ message: "Wrong email or password" });
-
-    }
-  })
-})
+  );
+});
 
 studentRouter.post("/enroll", (req, res) => {
   let teacherID = req.body.teacher;
@@ -85,14 +85,14 @@ studentRouter.post("/enroll", (req, res) => {
           { $addToSet: { students: [studentID] } },
           (err) => {
             if (err) {
-              res
-                .status(400)
-                .json({
-                  message: "Can't assign Student to Teacher ",
-                  error: err,
-                });
+              res.status(400).json({
+                message: "Can't assign Student to Teacher ",
+                error: err,
+              });
             } else {
-              res.status(200).json({ message: "Student Enrolled Successfully" });
+              res
+                .status(200)
+                .json({ message: "Student Enrolled Successfully" });
             }
           }
         );
@@ -101,39 +101,46 @@ studentRouter.post("/enroll", (req, res) => {
   );
 });
 
-
 studentRouter.post("/unenroll", (req, res) => {
   let teacherID = req.body.teacher;
   let studentID = req.body.student;
-
-  Student.updateOne(
-    { _id: studentID },
-    { $pullAll: { teachers: [teacherID] } },
-    (err) => {
-      if (err) {
-        res
-          .status(400)
-          .json({ message: "Can't Remove Teacher from Student", error: err });
-      } else {
-        Teacher.updateOne(
-          { _id: teacherID },
-          { $pullAll: { students: [studentID] } },
-          (err) => {
-            if (err) {
-              res
-                .status(400)
-                .json({
+  if (!teacherID) {
+    res.status(400).json({
+      message: "Incorrect Teacher ID ",
+    });
+  } else if (!studentID) {
+    res.status(400).json({
+      message: "Incorrect Student ID ",
+    });
+  } else
+    Student.updateOne(
+      { _id: studentID },
+      { $pullAll: { teachers: [teacherID] } },
+      (err) => {
+        if (err) {
+          res
+            .status(400)
+            .json({ message: "Can't Remove Teacher from Student", error: err });
+        } else {
+          Teacher.updateOne(
+            { _id: teacherID },
+            { $pullAll: { students: [studentID] } },
+            (err) => {
+              if (err) {
+                res.status(400).json({
                   message: "Can't Remove Student from Teacher ",
                   error: err,
                 });
-            } else {
-              res.status(200).json({ message: "Student UnEnrolled Successfully" });
+              } else {
+                res
+                  .status(200)
+                  .json({ message: "Student UnEnrolled Successfully" });
+              }
             }
-          }
-        );
+          );
+        }
       }
-    }
-  );
+    );
 });
 
 export default studentRouter;
