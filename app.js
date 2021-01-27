19423d414230ffebc2ed2283c892a65a93d8ab6e
MK1499 <m.khaled1499@gmail.com>
2021-01-27 20:54:11 +0200

    add socket event

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ const server = http.Server(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:4200",
+      "https://localhost:4200",
       "https://quranak-4a78a.web.app",
       "https://quranmk.herokuapp.com",
-      "http://192.168.1.8:400"
+      "http://192.168.1.8:400",
+      "*"
     ],
     methods: ["GET", "POST"],
     credentials: true,
